435fd3cf4bef946f698fb7b340ec3686879ab1a0
pockry <pockry@outlook.com>
2013-02-28 00:06:19 +0800

    dev 130227 night
    random song. working (or not add this
    song url. done
    song uploading. private, working

---
+++ music.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(0);
+if(isset($_GET["url"])){
+$opts = array(
+'http'=>array('method'=>"GET",'header'=>"User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.3)\r\n")
+);
+$context = stream_context_create($opts);
+$url = "http://www.vdisk.cn/down/index/".$_GET["url"];
+$data = file_get_contents($url,false,$context);
+preg_match("/name=.httpfileurl..content=.(.*?).>/", $data, $data);
+$myurl = $data[1];
+if($myurl){
+    header('Content-Type:application/force-download');
+    header("Location:".$myurl);
+die();
+}
+else
+echo "参数错误";
+};
+?>
