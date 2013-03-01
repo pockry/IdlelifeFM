4f8071844237353f198227c86fd421d5c50d8900
pockry <pockry@outlook.com>
2013-03-01 17:44:49 +0800

    dev 130301 daytime
    nearly do nothing

--- test.php
+++ test.php
@@ -18,6 +18,23 @@ aArr.sort(randomSort);
  alert(aArr);
  });
 </script>
- <body>
- </body>
+</head>
+<body>
+<?xml version="1.0" standalone="no"?>
+<!--Created with Sketsa SVG Editor (http://www.kiyut.com)-->
+<svg contentScriptType="text/ecmascript" width="600.0px" xmlns:xlink="http://www.w3.org/1999/xlink" baseProfile="full"
+    zoomAndPan="magnify" contentStyleType="text/css" height="600.0px" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg"
+    version="1.0">
+    <path fill="#333333" d="M 102.083 28.5 c -0.009 4.739 -3.844 8.574 -8.583 8.583 c -4.741 -0.009 -8.577 -3.844 -8.585 
+        -8.583 c 0.008 -4.741 3.844 -8.577 8.585 -8.585 c 1.913 0.0 3.665 0.629 5.09 1.686 l -1.782 1.783 l 8.429 2.256 
+        l -2.26 -8.427 l -1.89 1.89 c -2.072 -1.677 -4.717 -2.688 -7.587 -2.688 C 86.826004 16.418 81.418 21.826 81.416 
+        28.5 C 81.418 35.175 86.826004 40.583 93.5 40.583 S 105.583 35.175 105.583 28.5 H 102.083 z" stroke="#333333"/>
+    <path fill="#333333" d="M 37.132 22.971 c -2.203 -2.205 -5.916 -2.098 -8.25 0.235 L 28.5 23.588001 l -0.382 -0.382 c 
+        -2.334 -2.333 -6.047 -2.44 -8.25 -0.235 c -2.204 2.203 -2.098 5.916 0.235 8.249 l 8.396 8.396 l 8.396 -8.396 C 
+        39.229 28.887001 39.336 25.174 37.132 22.971 z" stroke="#333333"/>
+    <path fill="#333333" d="M 172.16699 27.5 L 172.16699 35.681 L 157.684 27.318 L 157.684 47.682 L 172.16699 39.318 L 
+        172.16699 47.5 L 176.5 47.5 L 176.5 27.5 z" stroke="#333333"/>
+</svg>
+
+</body>
 </html>
