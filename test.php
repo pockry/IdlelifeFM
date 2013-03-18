fa3a712cb9e0eca3f6cbec663b5a5572c50463f8
pockry <pockry@outlook.com>
2013-03-18 21:12:51 +0800

    restarted

--- test.php
+++ test.php
@@ -14,26 +14,136 @@ function randomSort(a,b){ //数组元素随机排列
     return Math.random()>.5 ? -1:1;
 }
 aArr.sort(randomSort);
- alert(aArr);
- });
+ //alert(aArr);
+ 
+ $(".playrow").hover(function(){$(this).find("path").attr("fill","#00ccff");},function(){$(this).find("path").attr("fill","#333333");});
+
 </script>
+<style type="text/css">
+#sample{
+	border:1px solid #666;
+	width:220px;
+	font-size:12px;
+	font-family:"Microsoft Yahei";
+	padding:0;
+	color:#333;
+	position:relative;
+}
+#sample legend{margin-left:10px;}
+
+#fm-artist{float:left;margin:0px 6px;font-size:18px;display:inline-block;overflow:hidden;width:170px; text-overflow:ellipsis;white-space:nowrap;}
+#fm-album{}
+#fm-from{color:#666;}
+#fm-title{display:block;color:#009966;margin-top:10px;margin-bottom:5px;}
+
+#progressbar{
+	margin:0px 5px;
+	width:210px;
+	height:2px;
+	background-color:#eee;
+}
+#played{
+	height:inherit;
+	background-color:#9dd6c5;
+	float:left;
+}
+#paused{
+	transform:rotate(90deg);
+	-ms-transform:rotate(90deg);
+	-webkit-transform:rotate(90deg);
+	-moz-transform:rotate(90deg);
+	display:inline-block;
+	float:right;
+	font-size:14px;
+	background-color:#9dd6c5;
+	padding:3px 6px 6px 6px;
+	margin:-9px 10px 0 0;
+	cursor:pointer;
+	color:white;
+	
+}
+#paused:hover{background-color:#b8f1e0;}
+#timevol{
+	text-align:right;
+	line-height:27px;
+	height:27px;
+	padding-right:5px;
+}
+#timeleft,#volbtn{
+	display:block;
+	float:right;
+	margin:auto 1px;
+	z-index:20;
+}
+#fm-like-i{
+	float:left;
+	margin-left:5px;
+	margin-top:7px;
+}
+
+#volbtn{font-size:18px;cursor:pointer;}
+#volumn{
+	float:right;
+	height:3px;
+	width:50px;
+	background-color:#eee;
+	cursor:pointer;
+	margin-top:12px;
+	margin-left:3px;
+	
+}
+#volumn-c{
+	height:inherit;
+	width:25px;
+	background-color:#333333;
+	cursor:pointer;
+}
+
+.playrow{
+	display:inline-block;
+	float:right;
+	margin:3px;
+	padding:3px;
+	border:1px solid #999;
+	text-align:center;
+	cursor:pointer;
+}
+.fm-ctn,.fm-songlist-p{
+	display:none;
+	z-index:100;
+	position:absolute;
+	text-align:center;
+	width:inherit;
+	height:184px;
+	line-height:184px;
+	vertical-align:middle;
+	margin-top:-8px;
+	background-color:rgba(255,255,255,0.6);
+	cursor:pointer;
+}
+.fm-songlist-p{
+	text-align:left;
+	line-height:1.5em;
+	cursor:default;
+	background-color:rgba(255,255,255,0.8);
+}
+.fm-songlist-q{border:1px solid #999;width:auto;height:auto;line-height:1.2em;float:left;margin:3px;cursor:pointer;padding:3px;text-align:center;}
+.fm-songlist-c{color:#ff0000;}
+#fm-songlist{position:absolute;bottom:3px;left:3px;border:1px solid #999;cursor:pointer;padding:3px;}
+.playrow:hover,#fm-songlist:hover,.fm-loop-c{color:#0cf;}
+.playrow span{display:block;height:25px;font-size:16px;vertical-align:middle;}
+.playrow span svg{margin-top:4px;}
+.clear{clear:both;}
+</style>
 </head>
+
 <body>
-<?xml version="1.0" standalone="no"?>
-<!--Created with Sketsa SVG Editor (http://www.kiyut.com)-->
-<svg contentScriptType="text/ecmascript" width="600.0px" xmlns:xlink="http://www.w3.org/1999/xlink" baseProfile="full"
-    zoomAndPan="magnify" contentStyleType="text/css" height="600.0px" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg"
-    version="1.0">
-    <path fill="#333333" d="M 102.083 28.5 c -0.009 4.739 -3.844 8.574 -8.583 8.583 c -4.741 -0.009 -8.577 -3.844 -8.585 
-        -8.583 c 0.008 -4.741 3.844 -8.577 8.585 -8.585 c 1.913 0.0 3.665 0.629 5.09 1.686 l -1.782 1.783 l 8.429 2.256 
-        l -2.26 -8.427 l -1.89 1.89 c -2.072 -1.677 -4.717 -2.688 -7.587 -2.688 C 86.826004 16.418 81.418 21.826 81.416 
-        28.5 C 81.418 35.175 86.826004 40.583 93.5 40.583 S 105.583 35.175 105.583 28.5 H 102.083 z" stroke="#333333"/>
-    <path fill="#333333" d="M 37.132 22.971 c -2.203 -2.205 -5.916 -2.098 -8.25 0.235 L 28.5 23.588001 l -0.382 -0.382 c 
-        -2.334 -2.333 -6.047 -2.44 -8.25 -0.235 c -2.204 2.203 -2.098 5.916 0.235 8.249 l 8.396 8.396 l 8.396 -8.396 C 
-        39.229 28.887001 39.336 25.174 37.132 22.971 z" stroke="#333333"/>
-    <path fill="#333333" d="M 172.16699 27.5 L 172.16699 35.681 L 157.684 27.318 L 157.684 47.682 L 172.16699 39.318 L 
-        172.16699 47.5 L 176.5 47.5 L 176.5 27.5 z" stroke="#333333"/>
+
+	<div id="fm-next" class="playrow"><span>
+	<svg width="26.134px" height="16px">
+<path fill="#333333" d="M22.756,0v6.4L11.377,0v6.4L0,0v16l11.377-6.4V16l11.378-6.4V16h3.378V0H22.756z"/>
 </svg>
+	</span>下一首</div>
 
 </body>
 </html>
