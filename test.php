34b2eded72cb0578659c0e89ac63fafc28b24069
pockry <pockry@outlook.com>
2013-03-19 14:34:53 +0800

    fix conflict

--- test.php
+++ test.php
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script src="js/jquery-1.9.1.min.js" type="text/javascript"></script>
-<title>html5-audio</title>
-<script type="text/javascript">
-$(document).ready(function(e) {
-var aArr = [];
-for(var i=0; i<100;i++){
-    aArr.push(i); //aArr[i] = i;
-}
-function randomSort(a,b){ //数组元素随机排列
-    return Math.random()>.5 ? -1:1;
-}
-aArr.sort(randomSort);
- //alert(aArr);
- });
-</script>
-</head>
-<body>
-
-</body>
-</html>
-=======
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -173,4 +147,3 @@ aArr.sort(randomSort);
 
 </body>
 </html>
->>>>>>> ae6f1356b81f144da6c4279ea97ad840040da719
