2c28c31139d2266b145f583130af0e48ed544b4e
pockry <pockry@outlook.com>
2015-07-27 20:14:57 +0800

    nearly work.

--- v2/js/FM_songlist_test.js
+++ v2/js/FM_songlist_test.js
@@ -1,8 +1,9 @@
 {
-  "title":"I S I",
-  "artist":"Duca",
-  "album":"\u3046\u305f\u3066\u3081\u3050\u308a \u30aa\u30ea\u30b8\u30ca\u30eb\u30b5\u30a6\u30f3\u30c9\u30c8\u30e9\u30c3\u30af ",
-  "tag":"2010-05",
-  "mp3":" ",
-  "webm":"none"
+  "title":"清楚帝国国歌～Tutti Piacere！",
+  "artist":"こばきょん",
+  "album":"con spirito",
+  "tag":"C85",
+  "mp3":"song/111.mp3",
+  "ogg":"song/111.ogg",
+  "songID": "112"
 }
